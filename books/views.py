b7b6ea382c58c0e98c5675c3d6dd0c4c0bd334f2
TrentHurlbut <trent.hurlbut@gmail.com>
2021-11-08 14:59:02 -0800

    books render and are ordered by created_at date

--- books/views.py
+++ books/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Book
 
 def home_page(request):
-  books = Book.objects.all()
+  books = Book.objects.order_by('created_at')
   return render(request, 'books/home_page.html', {"books":books})
